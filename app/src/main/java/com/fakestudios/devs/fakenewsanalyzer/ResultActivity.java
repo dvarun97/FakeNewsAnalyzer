7f2a0f386d0ed93029e2d9201f6b1926c6fde94c
dvarun97 <donivarun97@gmail.com>
2018-03-06 23:47:55 +0530

    basic

--- app/src/main/java/com/fakestudios/devs/fakenewsanalyzer/ResultActivity.java
+++ app/src/main/java/com/fakestudios/devs/fakenewsanalyzer/ResultActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
-    TextView resultTextview;
+    TextView resultTextview,type1Textview,type2Textview,type3Textview,notesTextview;
     Button homeButton;
 
     @Override
@@ -18,6 +18,10 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         resultTextview = (TextView) findViewById(R.id.result_textview);
+        type1Textview = (TextView) findViewById(R.id.result_type1_textview);
+        type2Textview = (TextView) findViewById(R.id.result_type2_textview);
+        type3Textview = (TextView) findViewById(R.id.result_type3_textview);
+        notesTextview = (TextView) findViewById(R.id.result_notes_textview);
         homeButton= (Button) findViewById(R.id.result_home_button);
 
         homeButton.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +32,23 @@ public class ResultActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        Intent intent = getIntent();
+
+        if(intent.getBooleanExtra("domainExists",false)){
+            resultTextview.setText(intent.getStringExtra("domain"));
+            type1Textview.setText(intent.getStringExtra("type1"));
+            type2Textview.setText(intent.getStringExtra("type2"));
+            type3Textview.setText(intent.getStringExtra("type3"));
+            notesTextview.setText(intent.getStringExtra("notes"));
+        }
+        else{
+            resultTextview.setText(intent.getStringExtra("domain"));
+            type1Textview.setText(intent.getStringExtra("type1"));
+            type2Textview.setText(intent.getStringExtra("type2"));
+            type3Textview.setText(intent.getStringExtra("type3"));
+            notesTextview.setText("Not found");
+        }
 
     }
 
--- app/src/main/java/com/fakestudios/devs/fakenewsanalyzer/SourceInputActivity.java
+++ app/src/main/java/com/fakestudios/devs/fakenewsanalyzer/SourceInputActivity.java
@@ -1,15 +1,30 @@
 package com.fakestudios.devs.fakenewsanalyzer;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Iterator;
 
 public class SourceInputActivity extends AppCompatActivity {
 
     Button analyzeBUtton,clearButton;
     EditText sourceEdittext;
+    String domain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +41,71 @@ public class SourceInputActivity extends AppCompatActivity {
                 sourceEdittext.setText("");
             }
         });
+
+        String url = sourceEdittext.getText().toString();
+        try{
+            domain=getDomainName(url);
+        }
+        catch (URISyntaxException e){
+            Toast.makeText(SourceInputActivity.this,"Invalid URL",Toast.LENGTH_SHORT);
+        }
+
+        try {
+            String jsonSources = AssetJSONFile("fakeSources.json", SourceInputActivity.this);
+            JSONObject sourceObj = new JSONObject(jsonSources);
+            Iterator iterator = sourceObj.keys();
+            JSONObject res=null;
+            String key=null;
+            boolean domainExists=false;
+            while(iterator.hasNext()){
+                key = (String)iterator.next();
+                if(key.equals(domain)){
+                    res = sourceObj.getJSONObject(key);
+                    domainExists=true;
+                    break;
+                }
+            }
+            if(domainExists){
+                displayResult(key,res.getString("type"),res.getString("2nd type"),res.getString("3rd type"),res.getString("Source Notes (things to know?)"),domainExists);
+            }
+            else{
+                displayResult(key,"","","","",domainExists);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();//no source file
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void displayResult(String domain, String type1, String type2, String type3, String notes,boolean domainExists) {
+        Intent intent = new Intent(SourceInputActivity.this,ResultActivity.class);
+        intent.putExtra("domain",domain);
+        intent.putExtra("type1",type1);
+        intent.putExtra("type2",type2);
+        intent.putExtra("type3",type3);
+        intent.putExtra("notes",notes);
+        intent.putExtra("domainExists",domainExists);
+        startActivity(intent);
+    }
+
+    public static String getDomainName(String url) throws URISyntaxException {
+        URI uri = new URI(url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public static String AssetJSONFile (String filename, Context context) throws IOException {
+        AssetManager manager = context.getAssets();
+        InputStream file = manager.open(filename);
+        byte[] formArray = new byte[file.available()];
+        file.read(formArray);
+        file.close();
+
+        return new String(formArray);
     }
 }
